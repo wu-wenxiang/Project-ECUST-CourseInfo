0cb8a96362f598cb732e96569d52821f5c6a00f6
Wu Wenxiang <wu.wenxiang@99cloud.net>
2019-11-03 19:40:42 +0800

    add admin.register

--- courseinfo/classroom/admin.py
+++ courseinfo/classroom/admin.py
@@ -3,18 +3,37 @@ from django.contrib import admin
 from .models import Campus, Building, ClassroomType, Classroom, Teacher, Term, Course
 
 
+@admin.register(Campus)
 class CampusAdmin(admin.ModelAdmin):
     list_display = ('name', 'show_schedule', 'show_classroom')
     list_editable = ['show_schedule', 'show_classroom']
 
+@admin.register(Building)
 class BuildingAdmin(admin.ModelAdmin):
     list_display = ('campus', 'name', 'show_schedule', 'show_classroom')
     list_editable = ['show_schedule', 'show_classroom']
 
+@admin.register(Classroom)
 class ClassroomAdmin(admin.ModelAdmin):
-    list_display = ('id', 'building', 'name', 'classroomType', 'show_schedule', 'show_classroom')
+    list_display = (
+        'id', 'building', 'name', 'classroomType', 'show_schedule',
+        'show_classroom')
     list_editable = ['show_schedule', 'show_classroom']
 
-admin.site.register(Campus, CampusAdmin)
-admin.site.register(Building, BuildingAdmin)
-admin.site.register(Classroom, ClassroomAdmin)
+@admin.register(ClassroomType)
+class ClassroomTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Term)
+class TermAdmin(admin.ModelAdmin):
+    list_display = ('name', 'firstMonday')
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'name', 'teacher', 'term', 'classroom', 'CLASS_TIME',
+        'START_TIME', 'XQ', 'KS', 'JS', 'ZC1', 'ZC2', 'SJBZ', 'showtext')
