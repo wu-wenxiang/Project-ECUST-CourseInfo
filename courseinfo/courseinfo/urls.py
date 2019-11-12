32c0eb3204d99d7e1dacd6190f9cc9bd87a2bbda
Wu Wenxiang <wu.wenxiang@99cloud.net>
2019-11-12 15:46:24 +0800

    tiny blank

--- courseinfo/courseinfo/urls.py
+++ courseinfo/courseinfo/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
-    path('', include('classroom.urls')),    
+    path('', include('classroom.urls')),
 ]
